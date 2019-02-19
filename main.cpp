4a2fc0748e7d0365b13848ed024324a3c1efe5ec
Nikita-Melnikov <36142899+Nikita-Melnikov@users.noreply.github.com>
2019-02-19 13:38:31 +0700

    Fix main.cpp & add reg functions
    w/ defines

--- main.cpp
+++ main.cpp
@@ -4,9 +4,25 @@
 #include <cmath>
 #include <string>
 
+/*
+#define 10 ;
+#define 11 ;
+#define 20 ;
+#define 21 ;
+#define 30 ;
+#define 31 ;
+#define 32 ;
+#define 33 ;
+*/
 int massive[100];//, value;
 
-int sc_memoryInit();
+int value;
+
+int flag; //registr = registr & (~(1 << (k вЂ“ 1)));
+
+int registr;
+
+int sc_memoryInit(void);
 
 int sc_memorySet(int adress, int value);
 
@@ -16,29 +32,40 @@ int sc_memorySave(char *filename);
 
 int sc_memoryLoad(char *filename);
 
+int sc_regInit(void);
+
+int sc_regSet(int register, int value);
+
+int sc_regGet(int register, int *value);
+
 int main(void)
 {
 	system("clear");
-	int adress, value;
-	char* File = "file.bin";
+	int adress;
+	//char registr;
+	char* File = "file.bin"; // warning
 	sc_memoryInit();
 	std::cout << "Input ur adress: ";
 	std::cin >> adress;
 	std::cout << std::endl;
-	int track_set = sc_memorySet(adress, value); // = value
-	int track_get = sc_memoryGet(adress, &value); // = *value
-	if(track_set != -1)
+	int memorySet = sc_memorySet(adress, value); // = value
+	int memoryGet = sc_memoryGet(adress, &value); // = *value
+	if(memorySet != -1 && memoryGet != -1)
 	{
-		std::cout << "Array[" << adress << "] = " << track_set << "\n";
-		std::cout << "Array[" << adress << "] has cell of memory: " << track_get << "\n";
+		std::cout << "Array[" << adress << "] = " << memorySet << "\n";
+		std::cout << "Array[" << adress << "] has cell of memory: " << &value << "\n";
 	}
 	sc_memorySave(&*File);
-	sc_memoryLoad(&*File);
+	int memoryLoad = sc_memoryLoad(&*File);
+	//if (memoryLoad != -1)
+	flag = sc_regInit();
+	sc_regSet(registr, value);
+	sc_regSet(registr, &value);
 	system("pause");
 	return 0;
 }
 
-int sc_memoryInit()
+int sc_memoryInit(void)
 {
 	for(auto i = 0; i < 100; i++)
 		massive[i] = 0;
@@ -62,7 +89,6 @@ int sc_memorySet(int adress, int value)
 
 int sc_memoryGet(int adress, int *value)
 {
-	int index;
 	if(adress > 99 || adress < 0)
 	{
 		std::cout << "Wrong number of memeory!(sc_memoryGet)\n";
@@ -70,26 +96,47 @@ int sc_memoryGet(int adress, int *value)
 	}
 	else
 	{
-		index = *value;
-		return index;
+		*value = massive[adress];
+		return *value;
 	}
 }
 
 int sc_memorySave(char *filename)
 {
-	std::ofstream fileStrmOut(filename, std::ios::binary );
-    fileStrmOut.write( ( char* ) &massive, sizeof(int) );
-    fileStrmOut.close();
+	std::ofstream file_open(filename, std::ios::binary);
+    file_open.write((char*) &massive, sizeof(int));
+    file_open.close();
 }
 
 int sc_memoryLoad(char *filename)
 {
-    std::ifstream fileStrmIn(filename, std::ios::binary );
-    while (fileStrmIn.read((char *) &massive, sizeof(int)))
+	int new_massive[100];
+    std::ifstream file_open(filename, std::ios::binary);
+    while(file_open.read((char *) &massive, sizeof(int)))
     {
     	for(auto i = 0; i < 100; i++)
-        std::cout <<"Array[" << i << "] = " << massive[i] << " ";
+        	file_open >> new_massive[i];
     }
+    file_open.close();
+    std::cout << "\n\nBinary vision of massive memory: \n";
+    for(auto i = 0; i < 100; i++)
+    	std::cout << "Array[" << i << "] = " << &new_massive[i] << "\t\t";
     std::cout << "\n";
-    fileStrmIn.close();
+    return 1;
+}
+
+int sc_regInit(void)
+{
+	flag = 0;
+	return 0;
+}
+
+int sc_regSet(int register, int value)
+{
+	//zadat' tipo registr i value i s nimi operacii(1)
+}
+
+int sc_regGet(int register, int *value)
+{
+	//
 }
